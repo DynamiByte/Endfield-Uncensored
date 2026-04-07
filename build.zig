91f93a53a0774880707dee71fee4a84ca206ccff
Leandro Baldassarre <leandro.baldassarre@icloud.com>
2026-04-07 18:01:46 -0400

    Remove loader tests from build process

--- build.zig
+++ build.zig
@@ -58,14 +58,4 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(exe);
 
-    const loader_tests = b.addTest(.{
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/loader.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    const run_loader_tests = b.addRunArtifact(loader_tests);
-    const test_step = b.step("test", "Run loader tests");
-    test_step.dependOn(&run_loader_tests.step);
 }
